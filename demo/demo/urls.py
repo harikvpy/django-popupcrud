dba9b3e5ad8f2b2add29ab62ebe3bc6436a4aebf
Hari Mahadevan <hari@smallpearl.com>
2017-09-13 02:38:07 +0000

    Rename testapp to demo & add demo homepage pointing to two its CRUDs

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('django.contrib.auth.urls', namespace='accounts')),
-    url(r'^library/', include('library.urls', namespace='library'))
+    url(r'^library/', include('library.urls', namespace='library')),
+    url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
 ]
